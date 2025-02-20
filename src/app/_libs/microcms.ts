cba27240c63dd2c767ada7a8a3659358fac78c56
yoshi0518 <yoshihumi.n.0518@gmail.com>
2025-02-20 19:47:49 +0900

    5章まで完了

--- src/app/_libs/microcms.ts
+++ src/app/_libs/microcms.ts
@@ -1,13 +1,62 @@
-export type News = {
-  id: string;
-  title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdAt: string;
-};
+import type { MicroCMSImage, MicroCMSListContent, MicroCMSQueries } from 'microcms-js-sdk';
+import { env } from '@/env';
+import { createClient } from 'microcms-js-sdk';
+
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
 
 export type Category = {
   name: string;
+} & MicroCMSListContent;
+
+export type News = {
+  title: string;
+  description: string;
+  content: string;
+  thumbnail?: MicroCMSImage;
+  category: Category;
+} & MicroCMSListContent;
+
+// export type News = {
+//   id: string;
+//   title: string;
+//   category: {
+//     name: string;
+//   };
+//   publishedAt: string;
+//   createdAt: string;
+// };
+
+// export type Category = {
+//   name: string;
+// };
+
+if (!env.MICROCMS_SERVICE_DOMAIN) throw new Error('MICROCMS_SERVICE_DOMAIN is required');
+if (!env.MICROCMS_API_KEY) throw new Error('MICROCMS_API_KEY is required');
+
+const client = createClient({
+  serviceDomain: env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: env.MICROCMS_API_KEY,
+});
+
+export const getMemberList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: 'members',
+    queries,
+  });
+
+  return listData;
+};
+
+export const getNewsList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<News>({
+    endpoint: 'news',
+    queries,
+  });
+
+  return listData;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,49 @@
-import type { News } from '@/app/_libs/microcms';
+// import type { News } from '@/app/_libs/microcms';
 import type { FC } from 'react';
 import Image from 'next/image';
 import { ButtonLink } from '@/app/_components/ButtonLink';
 import { NewsList } from '@/app/_components/NewsList';
+import { TOP_NEWS_LIMIT as limit } from '@/app/_constants';
+import { getNewsList } from '@/app/_libs/microcms';
 import styles from '@/app/page.module.css';
 
-const data: { contents: News[] } = {
-  contents: [
-    {
-      id: '1',
-      title: '渋谷にオフィスを移転しました',
-      category: {
-        name: '更新情報',
-      },
-      publishedAt: '2023/05/19',
-      createdAt: '2023/05/19',
-    },
-    {
-      id: '2',
-      title: '当社CEOが業界リーダーTOP30に選出されました',
-      category: {
-        name: '更新情報',
-      },
-      publishedAt: '2023/05/19',
-      createdAt: '2023/05/19',
-    },
-    {
-      id: '3',
-      title: 'テストの記事です',
-      category: {
-        name: '更新情報',
-      },
-      publishedAt: '2023/04/19',
-      createdAt: '2023/04/19',
-    },
-  ],
-};
+// const data: { contents: News[] } = {
+//   contents: [
+//     {
+//       id: '1',
+//       title: '渋谷にオフィスを移転しました',
+//       category: {
+//         name: '更新情報',
+//       },
+//       publishedAt: '2023/05/19',
+//       createdAt: '2023/05/19',
+//     },
+//     {
+//       id: '2',
+//       title: '当社CEOが業界リーダーTOP30に選出されました',
+//       category: {
+//         name: '更新情報',
+//       },
+//       publishedAt: '2023/05/19',
+//       createdAt: '2023/05/19',
+//     },
+//     {
+//       id: '3',
+//       title: 'テストの記事です',
+//       category: {
+//         name: '更新情報',
+//       },
+//       publishedAt: '2023/04/19',
+//       createdAt: '2023/04/19',
+//     },
+//   ],
+// };
 
-const Home: FC = () => {
-  const sliceData = data.contents.slice(0, 2);
+const Home: FC = async () => {
+  // const sliceData = data.contents.slice(0, 2);
+  const data = await getNewsList({
+    limit,
+  });
 
   return (
     <>
@@ -57,7 +62,7 @@ const Home: FC = () => {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <NewsList news={sliceData} />
+        <NewsList news={data.contents} />
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっとみる</ButtonLink>
         </div>
